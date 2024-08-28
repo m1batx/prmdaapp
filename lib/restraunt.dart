4d5e05e249157cb614b85243ae579ae8aa7143c7
m1batx <m1batx@github.com>
2024-08-28 20:12:53 +0300

    foodup

--- lib/restraunt.dart
+++ lib/restraunt.dart
@@ -134,6 +134,450 @@ class Restraunt extends ChangeNotifier{
         Addon(name: "Гриб", price: 50)
       ]
       ),
+      Food(
+      id: 7,
+      name: "Шаверма c креветками на тарелке",
+      ImagePath: "lib/images/angle_2/shrimp na tarelki 2.jpg",
+      price: 550,
+      Description: "Креветки в панировке (6 шт.), помидоры, пекинская капуста, солёные огурцы, лук красный, картошка и морковь по-корейски, чесночный соус и цезарь-соус",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 8,
+      name: "Шаверма в пите ",
+      ImagePath: "/lib/images/angle_2/v pite 2 .jpg",
+      price: 220,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 9,
+      name: "Шаверма в лаваше с грибами",
+      ImagePath: "lib/images/angle_2/classic with mashroom 2.jpg",
+      price: 280,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный, грибы",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 10,
+      name: "Шаверма в лаваше двойная",
+      ImagePath: "lib/images/angle_2/Dvainaya 2.jpg",
+      price: 300,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+        Food(
+      id: 11,
+      name: "Двойная шаверма на тарелке",
+      ImagePath: "lib/images/angle_2/classic hot  na tarelki 2.jpg",
+      price: 400,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный, морковь и картофель",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+       Food(
+      id: 12,
+      name: "Шаверма на тарелке",
+      ImagePath: "lib/images/angle_2/classic hot  na tarelki 2.jpg",
+      price: 350,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный, морковь и картофель",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+       Food(
+      id: 13,
+      name: "Египетская шаверма на тарелке",
+      ImagePath: "lib/images/angle_2/egeptskaya 2.jpg",
+      price: 300,
+      Description: " Курица, огурцы, помидоры, пекинская капуста, чесночный соус, гранатовый соус, солёные огурцы, красный лук, морковь и картофель",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 14,
+      name: "Двойная шаверма в пите",
+      ImagePath: "/lib/images/angle_2/v pite 2 .jpg",
+      price: 270,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 14,
+      name: "Шаверма в лаваше с сыром",
+      ImagePath: "lib/images/angle_2/Cirni lavash 2.jpg",
+      price: 280,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный, сыр",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 15,
+      name: "Супер острый в лаваше",
+      ImagePath: "lib/images/angle_2/classic hot 2.jpg",
+      price: 300,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, острый соус, солёные огурцы и халапеньо",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 15,
+      name: "Супер острый в лаваше",
+      ImagePath: "lib/images/angle_2/classic hot 2.jpg",
+      price: 300,
+      Description: "Курица, огурцы, помидоры, пекинская капуста, чесночный соус, острый соус, солёные огурцы и халапеньо",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 16,
+      name: "Хот-дог в лаваше",
+      ImagePath: "lib/images/angle_2/hotdog 2.jpg",
+      price: 200,
+      Description: "Сосиски, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 17,
+      name: "Двойной хот-дог в лаваше",
+      ImagePath: "lib/images/angle_2/hotdog 2.jpg",
+      price: 250,
+      Description: "Сосиски, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 18,
+      name: "Шаверма в лаваше говядина",
+      ImagePath: "lib/images/angle_2/govyadina 2.jpg",
+      price: 250,
+      Description: "Говядина, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 19,
+      name: "Шаверма на тарелке говядина",
+      ImagePath: "lib/images/angle_2/govyadina na tarelki 2.jpg",
+      price: 350,
+      Description: "Говядина, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный, морковь и картофель",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 20,
+      name: "Говяжья шаверма в лаваше двойная",
+      ImagePath: "lib/images/angle_2/Dvainaya 2.jpg",
+      price: 350,
+      Description: "Говядина, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 21,
+      name: "Чизбургер",
+      ImagePath: "lib/images.burger.ches burg.png",
+      price: 300,
+      Description: "бургер, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 22,
+      name: "Двойной чизбургер",
+      ImagePath: "lib/images/burger/doub.png",
+      price: 350,
+      Description: "бургер, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 23,
+      name: "Фалафель в пите",
+      ImagePath: "lib/images/angle_2/falafel v pite 2 .jpg",
+      price: 200,
+      Description: "Фалафель, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 24,
+      name: "Фалафель в лаваше",
+      ImagePath: "lib/images/angle_2/falafel 2.jpg",
+      price: 200,
+      Description: "Фалафель, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+      Food(
+      id: 24,
+      name: "Фалафель на тарелке",
+      ImagePath: "lib/images/angle_2/falafel na tarelki 2.jpg",
+      price: 200,
+      Description: "Фалафель 4шт, картошка, огурцы, помидоры, пекинская капуста, чесночный соус, солёные огурцы, лук красный",
+      meatType: MeatType.fish,
+      category: FoodCategory.shawarma,
+      availableAddons: [
+        Addon(name: "Огурцы маринованные", price: 50),
+        Addon(name: "Сыр 'Чеддер'", price: 75),
+        Addon(name: "Острый соус по египетски", price: 35),
+        Addon(name: "Халапеньо", price: 35),
+        Addon(name: "Кетчуп", price: 50),
+        Addon(name: "Больше чесночного соуса", price: 0),
+        Addon(name: "Картофель фри", price: 65),
+        Addon(name: "Барбекью соус", price: 50),
+        Addon(name: "Марковь по корейски", price: 65),
+        Addon(name: "Гриб", price: 50)
+      ]
+      ),
+
+
+
   ];
 
   //GETTER
